084cc107c72052e505c6cddb80e250a2e705d264
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-14 18:49:36 +0530

    added

---
+++ module 40. 5-1 do practice must/module-40-5-1.js
@@ -0,0 +1,15 @@
+/*
+*************************YOU HAVE TO MUST PRRACTICE************************
+
+1. show output after 3.5 seconds in anything by console.log
+2. take a number using prompt . add 200. then alert the result.
+3. purposes of cookies .  write 50 to 7 lines in english.
+4.  3 diferent between local storage and session storage.
+-------------               ---------------             ----------------
+------------------------------------------------------------------------------
+-----------------------------HOME WORK--------------------------
+
+6.  how JavaScript run ? write notes between 5 to 7 lines in english.
+7.  list of things thats anynchronous in js/browser. 
+8.  what is event loop in js and how does it work ? 
+*/
